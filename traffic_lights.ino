9747e78b457d0108e768ec0ba44c653524977373
rmoeller922 <78146913+rmoeller922@users.noreply.github.com>
2021-02-28 13:31:03 -0500

    Update traffic_lights.ino

--- traffic_lights.ino
+++ traffic_lights.ino
@@ -1,3 +1,4 @@
+//initializing pins and values
 int red = 8;
 int yellow = 6;
 int green = 4;
@@ -9,7 +10,9 @@ int timer = 0;
 bool Mode = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  
+  //setting up pins and configuring them as inputs or outputs
+  //also setting parameters of interrupt function
   pinMode(red, OUTPUT);
   pinMode(yellow, OUTPUT);
   pinMode(green, OUTPUT);
@@ -20,6 +23,8 @@ void setup() {
 }
 
 void buzz(){
+  
+  //buzz function buzzes buzzer for 3 seconds when called
    for(int i=0; i<500; i++){
     digitalWrite(buzzer, HIGH);
     delay(3);
@@ -29,23 +34,46 @@ void buzz(){
 }
 
 void cycleStart(){
+  
+  //cycleStart called at buttonPush and acts as a flag to exit while loop inside of loop() when called
   blinky = -1;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // main loop function
+  
   while(true){
+    
+    // timer is set to millis() which returns the amount of time that the program has been running
+    // millis() uses internal timer TIMER0 to track the milliseconds that the program has been running
     timer = millis();
+    
+    // if statements check to see if 500 milliseconds have passed or if ISR was called
     if( timer - newTime == blinky ){
+      
+      //when condition is met, newTime is set to Timer and the difference is checked to
+      //determine when 500 milliseconds has passed
       newTime = timer;
+      
+      // bool type variable Mode is set to its other state ( 1 or 0 )
       Mode = !Mode;
+      
+      // red LED is set to its other state (on or off)
       digitalWrite(red, Mode);
+      
     }
+    
+    // if ISR is called and flag is returned then program exits loop
     else if( blinky == -1){
+      
       break;
+      
     }
+    
   }
-
+  
+  // rest of traffic light program toggles through red, yellow, and green LEDS for set times
+  // and buzzes for 3 seconds before transitioning to the next state
   digitalWrite(red, HIGH);
   delay(12000);
   buzz();
@@ -60,4 +88,5 @@ void loop() {
   buzz();
 
   digitalWrite(yellow, LOW);
+  
 }
